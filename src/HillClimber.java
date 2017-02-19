ff257e169ff3d9931801696dfbb8b13006c803ef
Kyle <Kyle@DESKTOP-ILGFE70>
2017-02-18 22:58:27 -0500

    FUCK

--- src/HillClimber.java
+++ src/HillClimber.java
@@ -1,11 +1,11 @@
-
+import java.util.LinkedList;
 
 public class HillClimber {
 
 	private LocalSearchNode _current;
 	private int _shoulderSearchStepsAllowed;  // this is the number of "shoulder steps" allowed
-	// may need more instance variables
-	
+	private LinkedList<LocalSearchNode> _fringe; 
+
 	public static void main(String[] args) {
 		LocalSearchState initial = new StateClass(4);  // this should be a real LocalState (can change N)
 		HillClimber qc = new HillClimber(new LocalSearchNode(initial,0),0);
@@ -16,15 +16,36 @@ public class HillClimber {
 	public HillClimber(LocalSearchNode initial, int searchShoulder){
 		_current = initial;
 		_shoulderSearchStepsAllowed = searchShoulder;
+		_fringe.add(new LocalSearchNode(initial.getState(),0));
 	}
 
 	public LocalSearchNode findSolution() 
 	{
+		LocalSearchNode element;
+		LocalSearchState currentState;
+		LocalSearchState tempState;
+		int steps = 0;
+		// loop until find goal or run out of nodes to search from
+
 		while (true) 
-		{
-			// TODO  write the code in this loop
-			//       be sure that you return
-			return null;  // dummy return so this looks like working code
+		{ 
+			if (_fringe.isEmpty())
+				return null;
+			else 
+			{
+				element = _fringe.remove();
+				currentState = element.getState();
+				for (String s: element.getState().actions()) 
+				{
+					tempState = currentState.successor(s); 
+					if (currentState.value() != 0)
+					{
+						steps++;
+						_fringe.addFirst(new LocalSearchNode(tempState, steps));
+					}
+				}
+
+			}
 		}
 	}
 }
